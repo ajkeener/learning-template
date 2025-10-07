fcf13ab7691d01bff63e60fe84b383dd3b5c64a7
Avery Keener <averyjkeener@gmail.com>
2025-10-06 18:03:14 -0700

    createRoot notes, first component notes

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// How to write a component
 
+// First, imports
+import { useState } from 'react' 
+// not using this yet
+
+/* Component function formula
+function Name(){
+  statements, declarations, expressions
+  return (
+    Begin React Tree
+    Component, element, or fragment
+    <>
+    
+    </>
+  )
+} 
+  
+*/
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Hello World</h1>
+      <p>App.jsx</p>
     </>
-  )
+  );
 }
 
 export default App
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
+// main.jsx - where root node is applied
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+//App.jsx is just an example of a component
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
+  <StrictMode> {/* dev mode only, helps with bugs*/}
+    <App /> {/* main or first component */}
   </StrictMode>,
 )
+// createRoot(get the root element from document).render(elements or components);
+// you can render elements, components, and or fragments
+// there can only be 1 parent argument (any of the above as well)
+
